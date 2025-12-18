aa534e2ad6daee0436b90bcfedabe4a25964c1b5
woo122 <169125025+woo122@users.noreply.github.com>
2025-12-18 16:45:47 +0900

    Add files via upload

---
+++ build/resources/main/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO products
+(name, price, stock_quantity, image_url, category, featured, discounted)
+VALUES
+('인기상품1', 10000, 10, '/images/p1.jpg', 'CLOTHING', true, false),
+('인기상품2', 12000, 10, '/images/p2.jpg', 'CLOTHING', true, false),
+('인기상품3', 15000, 10, '/images/p3.jpg', 'CLOTHING', true, false),
+('인기상품1', 10000, 10, '/images/p4.jpg', 'CLOTHING', true, false),
+('인기상품2', 12000, 10, '/images/p5.jpg', 'CLOTHING', true, false),
+('인기상품3', 15000, 10, '/images/p6.jpg', 'CLOTHING', true, false),
+
+('할인상품1', 9000, 10, '/images/d1.jpg', 'ETC', false, true),
+('할인상품2', 8000, 10, '/images/d2.jpg', 'ETC', false, true),
+('할인상품3', 7000, 10, '/images/d3.jpg', 'ETC', false, true);
